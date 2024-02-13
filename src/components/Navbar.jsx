5a8a7575e55f7c5efb5864265ba3dc3329c77ac2
FareltXyz <fareltxyz@gmail.com>
2024-02-13 20:06:33 +0700

    add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar({ menu1, menu2 }) {
     return (
     <div className="navbar bg-transparent">
       <div className="flex-1 ml-3">
-          <Link className="text-xl font-bold text-[#eaeaea] hover:scale-125" href="/">Farelt<span className="text-[#c62707]">Xyz</span></Link>
+          <Link className="group/logo text-xl font-bold text-[#eaeaea] hover:scale-125" href="/">Farelt<span className="text-[#c62707] group-hover/logo:animate-rgb-color">Xyz</span></Link>
       </div>
       <div className="flex-none mr-3 flex flex-row space-x-3">
           <Link className="text-[#eaeaea] underline decoration-current hover:scale-90" href={"/" + menu2.toLowerCase()}>{menu2}</Link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,25 @@ module.exports = {
       'xl': '1280px',
      '2xl': '1536px'
     },
-    extend: {},
+    extend: {
+      keyframes: {
+        'rgbtransition': {
+          '10%': { color: '#c62707' },
+          '20%': { color: '#c2c507' },
+          '30%': { color: '#1dc507' },
+          '40%': { color: '#07c5c2' },
+          '50%': { color: '#1d07c5' },
+          '60%': { color: '#9207c5' },
+          '70%': { color: '#c207c5' },
+          '80%': { color: '#c50776' },
+          '90%': { color: '#c50740' },
+          '100%': { color: '#c62707' }
+        }
+      },
+      animation: {
+        'rgb-color': "rgbtransition 0.8s linear infinite"
+      }
+    },
   },
   plugins: [require("daisyui")],
 };
